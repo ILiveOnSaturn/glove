05aee2a8dac67d8ca129e166ffcb31a29fc716e1
I_live_on_saturn <iliveonsaturnanditsfun@gmail.com>
2023-01-18 12:39:59 +0200

    tinyusb is a pain

--- onboard code/glove/glove.c
+++ onboard code/glove/glove.c
@@ -14,10 +14,26 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
+        tud_task();
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
 #endif
+}
+
+void tud_mount_cb(void) {//invoked when device is mounted
+}
+
+void tud_umount_cb(void) {//invoked when device is unmounted
+}
+
+void tud_suspend_cb(bool remote_wakeup_en) { //invoked when usb bus is suspended. within 7ms device must draw and average current of less than 2.5mA from bus.
+    (void) remote_wakeup_en; //if host allows remote wake up
+    
+}
+
+void tud_resume_cb(void) { //invoked when usb bus is resumed.
+    
 }
